3dea54ef588bcf495919ff0cb70c5a49e706540a
sansil <sansil32@gmail.com>
2019-02-25 16:13:59 -0300

    archivos de produccion

---
+++ dist/precache-manifest.5b1481f1d80c501726bbafcf277726e4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "b2e94fc740ef35c9accd",
+    "url": "/js/chunk-vendors.8ae94e9a.js"
+  },
+  {
+    "revision": "2f569a142a1fd64ab073",
+    "url": "/js/app.d8a17ba7.js"
+  },
+  {
+    "revision": "31bf0e9b39aa3c930fa1",
+    "url": "/js/about.77c93d06.js"
+  },
+  {
+    "revision": "826e4ef66b4e4b328a19ec52762afaee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "b2e94fc740ef35c9accd",
+    "url": "/css/chunk-vendors.9ab6e0b6.css"
+  },
+  {
+    "revision": "2f569a142a1fd64ab073",
+    "url": "/css/app.019dc1e3.css"
+  }
+];
