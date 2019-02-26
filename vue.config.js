8a8fedb8bdfce6c9a01d52137056379a2e30ca4a
sansil <sansil32@gmail.com>
2019-02-26 11:21:59 -0300

    add deploy script

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   publicPath:
-    process.env.NODE_ENV === "production" ? "/busqueda_del_tesoro/" : "/"
+    process.env.NODE_ENV === "production" ? "busqueda_del_tesoro" : "/"
 };
