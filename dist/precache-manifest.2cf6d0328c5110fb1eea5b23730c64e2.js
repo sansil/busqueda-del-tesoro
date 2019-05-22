cea7556e43903acc066a7a38ec64e7d4c7f8a2c9
sansil <sansil32@gmail.com>
2019-05-22 15:51:42 -0300

    build files

---
+++ dist/precache-manifest.2cf6d0328c5110fb1eea5b23730c64e2.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "57bcd0d9fec0a2b8ede2fe787c43eed2",
+    "url": "/busqueda-del-tesoro/img/mapa.57bcd0d9.jpeg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/busqueda-del-tesoro/robots.txt"
+  },
+  {
+    "revision": "76b464c21d03f5ad1519488100f9fe49",
+    "url": "/busqueda-del-tesoro/img/ingenio.76b464c2.jpg"
+  },
+  {
+    "revision": "3febe74f0bafb7167c57",
+    "url": "/busqueda-del-tesoro/css/app.910ffb99.css"
+  },
+  {
+    "revision": "a6e09efe713a49338ae1",
+    "url": "/busqueda-del-tesoro/js/chunk-vendors.0dd9af37.js"
+  },
+  {
+    "revision": "aa50a063c416d9a44696994689c984fa",
+    "url": "/busqueda-del-tesoro/img/map3.aa50a063.png"
+  },
+  {
+    "revision": "c27764e559f76fc426a845da48904988",
+    "url": "/busqueda-del-tesoro/img/hojaVid.c27764e5.jpeg"
+  },
+  {
+    "revision": "da312025547ba9d2d3b31981ad1c95de",
+    "url": "/busqueda-del-tesoro/img/codigo_diccionario.da312025.png"
+  },
+  {
+    "revision": "c39f4c5bc9cc3e71805f9160ca500dbd",
+    "url": "/busqueda-del-tesoro/img/textura.c39f4c5b.png"
+  },
+  {
+    "revision": "77bb59852691ee0f177db605e4efb9c0",
+    "url": "/busqueda-del-tesoro/img/naranja_pista.77bb5985.jpeg"
+  },
+  {
+    "revision": "985d21cdd128353c5f6a",
+    "url": "/busqueda-del-tesoro/js/about.d1ccac1e.js"
+  },
+  {
+    "revision": "3febe74f0bafb7167c57",
+    "url": "/busqueda-del-tesoro/js/app.f00ce58b.js"
+  },
+  {
+    "revision": "fdd913ff42299da0834bfcb6e362c5fb",
+    "url": "/busqueda-del-tesoro/img/test2.fdd913ff.png"
+  },
+  {
+    "revision": "74e965f448cc48dcc6384681666563b0",
+    "url": "/busqueda-del-tesoro/img/sause_pista.74e965f4.jpeg"
+  },
+  {
+    "revision": "850c3f8a735c24af42a309896bed1bee",
+    "url": "/busqueda-del-tesoro/img/Preboda-36.850c3f8a.jpg"
+  },
+  {
+    "revision": "99462426a539d6a26fd495da30475bcf",
+    "url": "/busqueda-del-tesoro/index.html"
+  },
+  {
+    "revision": "03e43755cc9d68b9e8c326dfae0ccc15",
+    "url": "/busqueda-del-tesoro/pistas/grupoAzul.json"
+  },
+  {
+    "revision": "e69f251c275c9d0110a9835207332a2a",
+    "url": "/busqueda-del-tesoro/pistas/grupoRojo.json"
+  },
+  {
+    "revision": "ca00200b8bb599e9342a2b4e6febcbf3",
+    "url": "/busqueda-del-tesoro/pistas/grupoVerde.json"
+  },
+  {
+    "revision": "985d21cdd128353c5f6a",
+    "url": "/busqueda-del-tesoro/css/about.dbc38d35.css"
+  }
+];
